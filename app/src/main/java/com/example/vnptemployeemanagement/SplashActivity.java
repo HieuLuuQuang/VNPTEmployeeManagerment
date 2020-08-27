0675b9d103ae38ebac847b96059a44777ae3327e
HieuLuu <hieuluuptit@gmail.com>
2020-08-27 11:18:19 +0700

    add splashscreen, departments

---
+++ app/src/main/java/com/example/vnptemployeemanagement/SplashActivity.java
@@ -0,0 +1,36 @@
+package com.example.vnptemployeemanagement;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        new Handler(Looper.myLooper()).postDelayed(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent intent = new Intent(SplashActivity.this, HomeActivity.class);
+                        startActivity(intent);
+                    }
+                },2000
+        );
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+}
